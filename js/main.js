e837aecc7135bdfd06e2890541c4086938c920be
Sunset-Kim <wood2002@naver.com>
2021-05-08 22:45:02 +0900

    🎨 새로고침, 리사이즈시 현재의 index값을 결정

--- js/main.js
+++ js/main.js
@@ -55,6 +55,18 @@
             sceneInfo[i].objs.container.style.height =
                 sceneInfo[i].scrollHeight + 'px';
         }
+
+        yOffset = window.pageYOffset;
+        let totalScrollHeight = 0;
+        for(let i =0; i < sceneInfo.length; i++) {
+            totalScrollHeight += sceneInfo[i].scrollHeight;
+            if(totalScrollHeight >= yOffset) {
+                currentScene = i;
+                break; //반복문을 멈추고 빠져나옴 없을시 무조건 currentidex가 3이 된다.
+            }
+        }
+        document.body.setAttribute('id',`show-scene-${currentScene}`);
+
     }
 
     function scrollLoop() {
@@ -67,23 +79,29 @@
 
         if(yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
             currentScene ++;
+            document.body.setAttribute('id',`show-scene-${currentScene}`);
+
         } else if (yOffset < prevScrollHeight) {
             if(currentScene === 0) return;
             currentScene --;
+            document.body.setAttribute('id',`show-scene-${currentScene}`);
         }
-
+        // document.body.setAttribute('id',`show-scene-${currentScene}`);
         console.log(currentScene);
     }
 
     // 이벤트
+    // load와 돔컨텐트 로드 차이 => html 구조만 다 받으면,
+    window.addEventListener('load',()=>{
+        setLayout();
+    })
     window.addEventListener('resize', setLayout)
     window.addEventListener('scroll', () => {
         yOffset = pageYOffset;
-        scrollLoop()
+        scrollLoop();
     })
 
     // tdd 코드
-    setLayout()
     scrollLoop();
 
 
